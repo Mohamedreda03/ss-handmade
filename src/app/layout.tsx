b343522bafdca80d0639a03b1914727ee362b34f
Mohamed Reda <mo03.reda@gmail.com>
2025-04-18 13:19:19 +0200

    ff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,52 +10,52 @@ import localFont from "next/font/local";
 import { cn } from "@/lib/utils";
 import { GoogleAnalytics } from "@next/third-parties/google";
 
-import { Cairo } from "next/font/google";
+// import { Cairo } from "next/font/google";
 
-const cairo = Cairo({
-  weight: ["200", "300", "400", "500", "600", "700", "800", "900", "1000"],
-  subsets: ["arabic"],
-});
-
-// const dgagnadeen = localFont({
-//   src: [
-//     {
-//       path: "../fonts/alfont_com_DGAgnadeen-Thin.ttf",
-//       weight: "100",
-//       style: "normal",
-//     },
-//     {
-//       path: "../fonts/alfont_com_DGAgnadeen-Ultralight.ttf",
-//       weight: "200",
-//       style: "normal",
-//     },
-//     {
-//       path: "../fonts/alfont_com_DGAgnadeen-Light.ttf",
-//       weight: "300",
-//       style: "normal",
-//     },
-//     {
-//       path: "../fonts/alfont_com_DGAgnadeen-Regular.ttf",
-//       weight: "400",
-//       style: "normal",
-//     },
-//     {
-//       path: "../fonts/alfont_com_DGAgnadeen-Bold.ttf",
-//       weight: "500",
-//       style: "normal",
-//     },
-//     {
-//       path: "../fonts/alfont_com_DGAgnadeen-Extrabold.ttf",
-//       weight: "600",
-//       style: "normal",
-//     },
-//     {
-//       path: "../fonts/alfont_com_DGAgnadeen-Heavy.ttf",
-//       weight: "700",
-//       style: "normal",
-//     },
-//   ],
+// const cairo = Cairo({
+//   weight: ["200", "300", "400", "500", "600", "700", "800", "900", "1000"],
+//   subsets: ["arabic"],
 // });
+
+const dgagnadeen = localFont({
+  src: [
+    {
+      path: "../fonts/alfont_com_DGAgnadeen-Thin.ttf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../fonts/alfont_com_DGAgnadeen-Ultralight.ttf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../fonts/alfont_com_DGAgnadeen-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../fonts/alfont_com_DGAgnadeen-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../fonts/alfont_com_DGAgnadeen-Bold.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../fonts/alfont_com_DGAgnadeen-Extrabold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../fonts/alfont_com_DGAgnadeen-Heavy.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
 
 export const metadata: Metadata = {
   title: "handmade",
@@ -86,7 +86,7 @@ export default function RootLayout({
     <html lang="ar" dir="rtl">
       <AuthChecker>
         <Providers>
-          <body className={cn(cairo.className, "dark:bg-dark_background")}>
+          <body className={cn(dgagnadeen.className, "dark:bg-dark_background")}>
             <NextTopLoader height={3} showSpinner={false} />
             <ThemeProvider
               attribute="light"
